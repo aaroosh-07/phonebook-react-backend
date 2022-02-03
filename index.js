3fa770f2a7e6afc3fb0deeafb95fae840e8852ca
Aaroosh Agarwal <aaroosh.2012@gmail.com>
2022-02-03 23:22:28 +0530

    api boilerplate

---
+++ index.js
@@ -0,0 +1,73 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        num: "040-123456",
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        num: "39-44-5323523",
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        num: "12-43-234345",
+    },
+    {
+        id: 4,
+        name: "Mary Poppendieck",
+        num: "39-23-6423122",
+    },
+];
+
+const generateId = () => {
+    const maxId = Math.max(...persons.map((a) => a.id));
+    return maxId + 1;
+};
+
+app.get("/api/persons", (request, response) => {
+    response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find((person) => person.id === id);
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+});
+
+app.post("/api/persons", (request, response) => {
+    const body = request.body;
+    if (!body.name || !body.num) {
+        response.status(400).end();
+    }
+    const person = {
+        id: generateId(),
+        name: body.name,
+        num: body.num,
+    };
+    persons = persons.concat(person);
+    response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter((p) => p.id !== id);
+
+    response.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
