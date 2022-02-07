52f485f989bab17d278e12abb3d853d1c0865cad
Aaroosh Agarwal <aaroosh.2012@gmail.com>
2022-02-07 22:07:28 +0530

    completes api functionality

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
@@ -10,21 +13,25 @@ let persons = [
         id: 1,
         name: "Arto Hellas",
         num: "040-123456",
+        important: false,
     },
     {
         id: 2,
         name: "Ada Lovelace",
         num: "39-44-5323523",
+        important: false,
     },
     {
         id: 3,
         name: "Dan Abramov",
         num: "12-43-234345",
+        important: false,
     },
     {
         id: 4,
         name: "Mary Poppendieck",
         num: "39-23-6423122",
+        important: false,
     },
 ];
 
@@ -56,9 +63,30 @@ app.post("/api/persons", (request, response) => {
         id: generateId(),
         name: body.name,
         num: body.num,
+        important: body.important,
     };
     persons = persons.concat(person);
     response.json(person);
+});
+
+app.put("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const per = persons.find((p) => p.id === id);
+    if (!per) {
+        res.status(400).end();
+    }
+    const body = req.body;
+    if (!body.name || !body.num) {
+        res.status(400).end();
+    }
+    const person = {
+        id: id,
+        name: body.name,
+        num: body.num,
+        important: body.important,
+    };
+    persons = persons.map((p) => (p.id === id ? person : p));
+    res.status(200).end();
 });
 
 app.delete("/api/persons/:id", (request, response) => {
